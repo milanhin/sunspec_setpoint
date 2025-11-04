aac81773b1c6ceb3ea69c6b329b45dcb0f5b6fa5
Milan Hintjens <milanhin@gmail.com>
2025-11-04 12:07:50 +0100

    Added unit handling of power entities used for input

--- custom_components/sunspec_setpoint/const.py
+++ custom_components/sunspec_setpoint/const.py
@@ -4,3 +4,4 @@ CONF_PWR_IMP_ENT_ID = "power_import_entity_id"
 CONF_PWR_EXP_ENT_ID = "power_export_entity_id"
 CONF_PWR_PV_ENT_ID = "power_PV_entity_id"
 INJ_CUTOFF_TARIFF = 50  # [€/MwH]
+CONF_PWR_PV_MAX = "power_PV_max"  # [W]
--- custom_components/sunspec_setpoint/number.py
+++ custom_components/sunspec_setpoint/number.py
@@ -2,9 +2,10 @@ import logging
 import voluptuous as vol
 
 from datetime import timedelta
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, State
 from homeassistant.components.number import NumberEntity, PLATFORM_SCHEMA
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.const import UnitOfPower
 from .const import(
@@ -13,6 +14,7 @@ from .const import(
     CONF_PWR_EXP_ENT_ID,
     CONF_PWR_PV_ENT_ID,
     INJ_CUTOFF_TARIFF,
+    CONF_PWR_PV_MAX,
 )   
 
 _LOGGER = logging.getLogger(__name__)
@@ -20,10 +22,11 @@ SCAN_INTERVAL = timedelta(seconds=60)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        vol.Required(CONF_INJ_TARIFF_ENT_ID): str,
-        vol.Required(CONF_PWR_IMP_ENT_ID): str,
-        vol.Required(CONF_PWR_EXP_ENT_ID): str,
-        vol.Required(CONF_PWR_PV_ENT_ID): str
+        vol.Required(CONF_INJ_TARIFF_ENT_ID): cv.string,
+        vol.Required(CONF_PWR_IMP_ENT_ID): cv.string,
+        vol.Required(CONF_PWR_EXP_ENT_ID): cv.string,
+        vol.Required(CONF_PWR_PV_ENT_ID): cv.string,
+        vol.Required(CONF_PWR_PV_MAX): vol.Coerce(int),
     }
 )
 
@@ -38,8 +41,9 @@ async def async_setup_platform(
     pwr_ent_ids[CONF_PWR_IMP_ENT_ID] = config[CONF_PWR_IMP_ENT_ID]
     pwr_ent_ids[CONF_PWR_EXP_ENT_ID] = config[CONF_PWR_EXP_ENT_ID]
     pwr_ent_ids[CONF_PWR_PV_ENT_ID] = config[CONF_PWR_PV_ENT_ID]
+    pwr_pv_max = config[CONF_PWR_PV_MAX]
 
-    async_add_entities([SetpointNumber(hass, pwr_ent_ids)])
+    async_add_entities([SetpointNumber(hass, pwr_ent_ids, pwr_pv_max)])
     _LOGGER.info("SunSpec Setpoint platform was set up")
     
 class SetpointNumber(NumberEntity):
@@ -48,11 +52,17 @@ class SetpointNumber(NumberEntity):
     _attr_native_min_value = 0
     _attr_native_max_value = 1000
 
-    def __init__(self, hass: HomeAssistant, pwr_ent_ids: dict[str, str]) -> None:
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        pwr_ent_ids: dict[str, str],
+        pwr_pv_max: int,
+    ) -> None:
         super().__init__()
 
         self.pwr_ent_ids = pwr_ent_ids
         self.hass = hass
+        self.pwr_pv_max = pwr_pv_max
 
     async def async_set_native_value(self, value: float) -> None:
         self._attr_native_value = value
@@ -64,24 +74,45 @@ class SetpointNumber(NumberEntity):
         pwr_export_state = self.hass.states.get(self.pwr_ent_ids[CONF_PWR_EXP_ENT_ID])
         pwr_PV_state = self.hass.states.get(self.pwr_ent_ids[CONF_PWR_PV_ENT_ID])
 
-        inj_tariff = float(inj_tariff_state.state) # pyright: ignore[reportOptionalMemberAccess]
-        pwr_import = float(pwr_import_state.state) # pyright: ignore[reportOptionalMemberAccess]
-        pwr_export = float(pwr_export_state.state) # pyright: ignore[reportOptionalMemberAccess]
-        pwr_PV     = float(pwr_PV_state.state)     # pyright: ignore[reportOptionalMemberAccess]
+        # temporary type fix
+        if inj_tariff_state == None: return None
+        if pwr_import_state == None: return None
+        if pwr_export_state == None: return None
+        if pwr_PV_state == None: return None
+
+        inj_tariff = self.convert_pwr_state_to_watt(inj_tariff_state)
+        pwr_import = self.convert_pwr_state_to_watt(pwr_import_state)
+        pwr_export = self.convert_pwr_state_to_watt(pwr_export_state)
+        pwr_PV     = self.convert_pwr_state_to_watt(pwr_PV_state)
 
         setpoint = self.calc_setpoint(inj_tariff, pwr_import, pwr_export, pwr_PV)
         await self.async_set_native_value(setpoint)
 
 
-    def calc_setpoint(self, inj_tariff, pwr_import, pwr_export, pwr_PV) -> float:
-        max_PV_power = 4000  # assumed max (needs to be fetched from sunspec)
+    def calc_setpoint(self, inj_tariff: float, pwr_import: float, pwr_export: float, pwr_PV: float) -> float:
+        max_PV_power = self.pwr_pv_max
         if inj_tariff >= INJ_CUTOFF_TARIFF:
             return round(max_PV_power)
         
-        if pwr_import > 0 and inj_tariff < INJ_CUTOFF_TARIFF:
+        if pwr_import > 20 and inj_tariff < INJ_CUTOFF_TARIFF:
             sp = min(pwr_PV + pwr_import, max_PV_power)  # don't go above max power
+            _LOGGER.info(f"PV setpoint sent during curtailing and importing from grid, PV: {pwr_PV} W, import: {pwr_import} W, export: {pwr_export} W, setpoint: {sp} W")
             return round(sp)
 
         else: # injecting during negative price
             sp = max(pwr_PV - pwr_export, 0)  # no negative power
+            _LOGGER.info(f"PV setpoint sent during curtailing and exporting to grid, PV: {pwr_PV} W, import: {pwr_import} W, export: {pwr_export} W, setpoint: {sp} W")
             return round(sp)
+    
+    def convert_pwr_state_to_watt(self, state: State) -> float:
+        value = float(state.state)
+        unit = state.attributes.get("unit_of_measurement", None)
+        if unit == "kW":
+            return value * 1e3
+        elif unit == "W":
+            return value
+        elif unit == "mW" or unit == "MW":
+            return value / 1e3
+        else:
+            _LOGGER.error(f"Provided power entity {state.entity_id} has to known unit")
+            return 0
